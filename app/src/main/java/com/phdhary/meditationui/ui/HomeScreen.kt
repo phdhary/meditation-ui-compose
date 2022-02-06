9625e0ddff5f96178e521f2dfd921be04bcc6659
Hary <phdhary@gmail.com>
2022-02-06 21:34:28 +0700

    add currentMeditation section

--- app/src/main/java/com/phdhary/meditationui/ui/HomeScreen.kt
+++ app/src/main/java/com/phdhary/meditationui/ui/HomeScreen.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
@@ -16,10 +17,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import com.phdhary.meditationui.R
-import com.phdhary.meditationui.ui.theme.ButtonBlue
-import com.phdhary.meditationui.ui.theme.DarkerButtonBlue
-import com.phdhary.meditationui.ui.theme.DeepBlue
-import com.phdhary.meditationui.ui.theme.TextWhite
+import com.phdhary.meditationui.ui.theme.*
 
 @Composable
 fun HomeScreen() {
@@ -31,6 +29,7 @@ fun HomeScreen() {
         Column {
             GreetingSection()
             ChipSection(chips = listOf("Sweet sleep", "Insomnia", "Depression"))
+            CurrentMeditation()
         }
     }
 }
@@ -94,4 +93,47 @@ fun ChipSection(
         }
     }
 
+}
+
+
+@Composable
+fun CurrentMeditation(
+    color: Color = LightRed
+) {
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceBetween, modifier = Modifier
+            .padding(15.dp)
+            .clip(RoundedCornerShape(10.dp))
+            .background(color)
+            .padding(horizontal = 15.dp, vertical = 20.dp)
+            .fillMaxWidth()
+    ) {
+        Column {
+            Text(
+                text = "Daily Thought",
+                style = MaterialTheme.typography.h2
+            )
+            Text(
+                text = "Meditation - 3-10 min",
+                style = MaterialTheme.typography.body1,
+                color = TextWhite,
+            )
+        }
+        Box(
+            contentAlignment = Alignment.Center,
+            modifier = Modifier
+                .size(40.dp)
+                .clip(CircleShape)
+                .background(ButtonBlue)
+                .padding(10.dp)
+        ) {
+            Icon(
+                painter = painterResource(id = R.drawable.ic_play),
+                contentDescription = "Play",
+                tint = Color.White,
+                modifier = Modifier.size(16.dp)
+            )
+        }
+    }
 }
